d168ee0c9ee11cd7bb5fd4c5b78403a6ad248ad6
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-04-30 11:13:44 -0300

    Documentação do endpoint /api/adoptions

--- src/main/java/br/com/emendes/adopetapi/controller/swagger/AdoptionControllerSwagger.java
+++ src/main/java/br/com/emendes/adopetapi/controller/swagger/AdoptionControllerSwagger.java
@@ -1,6 +1,7 @@
 package br.com.emendes.adopetapi.controller.swagger;
 
 import br.com.emendes.adopetapi.dto.request.AdoptionRequest;
+import br.com.emendes.adopetapi.dto.request.UpdateStatusRequest;
 import br.com.emendes.adopetapi.dto.response.AdoptionResponse;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,21 +11,109 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@OpenAPIDefinition(tags = @Tag(name = "Adoption"), security = {@SecurityRequirement(name = "bearer-key")})
+@OpenAPIDefinition(
+    tags = @Tag(name = "Adoption", description = "Adoption management APIs"),
+    security = {@SecurityRequirement(name = "bearer-key")})
 public interface AdoptionControllerSwagger {
 
-  @Operation(summary = "Request an adoption", description = "xalala", tags = {"Adoption"})
+  @Operation(
+      summary = "Request an adoption",
+      description = "Request an adoption by specifying a petId on request body. The response, if successful, is a DTO object with information about requested adoption.",
+      tags = {"Adoption"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "201", description = "Successful adoption request",
           content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdoptionResponse.class))),
       @ApiResponse(responseCode = "400", description = "Something is wrong with the request",
           content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDetail.class))),
-          @ApiResponse(responseCode = "401", description = "Unauthorized, client did not send authorization header or sent an invalid one", content = @Content),
-          @ApiResponse(responseCode = "403", description = "Forbidden, User does not have access permission", content = @Content),
-          })
-      ResponseEntity < AdoptionResponse > adopt(AdoptionRequest adoptionRequest, UriComponentsBuilder uriComponentsBuilder);
+      @ApiResponse(responseCode = "401", description = "Unauthorized, client did not send authorization header or sent an invalid one", content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden, User does not have access permission", content = @Content),
+  })
+  ResponseEntity<AdoptionResponse> adopt(AdoptionRequest adoptionRequest, UriComponentsBuilder uriComponentsBuilder);
+
+  @Operation(
+      summary = "Fetch Page of adoption",
+      description = "Fetch paged adoption related to current user",
+      tags = {"Adoption"})
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successful fetch",
+          content = @Content),
+      @ApiResponse(
+          responseCode = "401",
+          description = "Unauthorized, client did not send authorization header or sent an invalid one",
+          content = @Content)
+  })
+  ResponseEntity<Page<AdoptionResponse>> fetchAll(@ParameterObject Pageable pageable);
+
+  @Operation(
+      summary = "Retrieve details about a adoption by id",
+      description = "Get Adoption related to current user by specifying its id. The response is AdoptionResponse with id, petId, guardianId, status and date.",
+      tags = {"Adoption"})
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successful retrieve",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdoptionResponse.class))),
+      @ApiResponse(responseCode = "400", description = "Something is wrong with the request",
+          content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDetail.class))),
+      @ApiResponse(
+          responseCode = "401",
+          description = "Unauthorized, client did not send authorization header or sent an invalid one",
+          content = @Content),
+      @ApiResponse(
+          responseCode = "404",
+          description = "Adoption not found for current user",
+          content = @Content)
+  })
+  ResponseEntity<AdoptionResponse> findById(Long id);
+
+  @Operation(
+      summary = "Update status of an adoption",
+      description = "Update status of an Adoption related to current user by specifying its id on path and new status on request body.",
+      tags = {"Adoption"})
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200", description = "Successful update",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdoptionResponse.class))),
+      @ApiResponse(
+          responseCode = "400", description = "Something is wrong with the request",
+          content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDetail.class))),
+      @ApiResponse(
+          responseCode = "401",
+          description = "Unauthorized, client did not send authorization header or sent an invalid one",
+          content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden, User does not have access permission", content = @Content),
+      @ApiResponse(
+          responseCode = "404",
+          description = "Adoption not found for current user",
+          content = @Content)
+  })
+  ResponseEntity<AdoptionResponse> updateStatus(Long id, UpdateStatusRequest updateStatusRequest);
+
+  @Operation(
+      summary = "Delete adoption by id",
+      description = "Delete Adoption related to current user by specifying its id.",
+      tags = {"Adoption"})
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200", description = "Successful delete",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = AdoptionResponse.class))),
+      @ApiResponse(
+          responseCode = "400", description = "Something is wrong with the request",
+          content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDetail.class))),
+      @ApiResponse(
+          responseCode = "401",
+          description = "Unauthorized, client did not send authorization header or sent an invalid one",
+          content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden, User does not have access permission", content = @Content),
+      @ApiResponse(
+          responseCode = "404",
+          description = "Adoption not found for current user",
+          content = @Content)
+  })
+  ResponseEntity<Void> delete(Long id);
 }
